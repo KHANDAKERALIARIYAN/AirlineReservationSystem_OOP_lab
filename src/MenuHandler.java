5a7042dff61debe433e86b72c0e7425363e99437
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-03-26 16:00:43 +0600

    changed the method and also changed the switch cases

--- src/MenuHandler.java
+++ src/MenuHandler.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuHandler {
@@ -8,36 +9,48 @@ public class MenuHandler {
 
     public void start() {
         menu.displayWelcomeMessage();
-        int desiredOption;
-
         try (Scanner scanner = new Scanner(System.in)) {
+            int desiredOption;
             do {
                 menu.displayMainMenu();
-                desiredOption = scanner.nextInt();
-
-                switch (desiredOption) {
-                    case 1:
-                        auth.handleAdminLogin(scanner, customerManager, flightManager);
-                        break;
-                    case 2:
-                        auth.registerAdmin(scanner);
-                        break;
-                    case 3:
-                        auth.handlePassengerLogin(scanner, flightManager);
-                        break;
-                    case 4:
-                        customerManager.registerPassenger(scanner);
-                        break;
-                    case 5:
-                        menu.displayManualInstructions(scanner);
-                        break;
-                    case 0:
-                        System.out.println("Thanks for Using BAV Airlines Ticketing System...!!!");
-                        break;
-                    default:
-                        System.out.println("Invalid Option. Please try again.");
-                }
+                desiredOption = getUserOption(scanner);
+                handleMenuOption(desiredOption, scanner);
             } while (desiredOption != 0);
+        }
+    }
+
+    private int getUserOption(Scanner scanner) {
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            scanner.nextLine(); // Clear invalid input
+            System.out.println("Invalid input. Please enter a valid number.");
+            return -1; // Return an invalid option to prompt retry
+        }
+    }
+
+    private void handleMenuOption(int option, Scanner scanner) {
+        switch (option) {
+            case 1:
+                auth.handleAdminLogin(scanner, customerManager, flightManager);
+                break;
+            case 2:
+                auth.registerAdmin(scanner);
+                break;
+            case 3:
+                auth.handlePassengerLogin(scanner, flightManager);
+                break;
+            case 4:
+                customerManager.registerPassenger(scanner);
+                break;
+            case 5:
+                menu.displayManualInstructions(scanner);
+                break;
+            case 0:
+                System.out.println("Thanks for Using BAV Airlines Ticketing System...!!!");
+                break;
+            default:
+                System.out.println("Invalid Option. Please try again.");
         }
     }
 }
